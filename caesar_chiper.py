6e51a307e0f0f33441905d1de19e0cd7583509ea
diacarcor <>
2021-05-15 16:52:04 +0000

    Eliminating index validation and duplicating alphabet

--- caesar_chiper.py
+++ caesar_chiper.py
@@ -7,9 +7,6 @@ def encrypt(input_text,shift_amount):
 
         new_position = position + shift_amount
 
-        if new_position >=  alphabet.alphabet_len:
-            new_position -= alphabet.alphabet_len
-        
         encoded_text += alphabet.alphabet_list[new_position]
     
     print("The encoded text is " + encoded_text)
@@ -21,9 +18,6 @@ def decrypt (input_text,shift_amount):
         position = alphabet.alphabet_list.index(character) 
 
         new_position = position - shift_amount
-
-        if new_position < 0:
-            new_position += alphabet.alphabet_len
 
         plain_text += alphabet.alphabet_list[new_position]
 
